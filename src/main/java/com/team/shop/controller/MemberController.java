47291871131296a28a0ed05004a7a6b29cef7b1f
hr.seo <hr.seo@kakao.com>
2021-09-06 09:45:57 +0900

    아이디 중복 검사 구현

--- src/main/java/com/team/shop/controller/MemberController.java
+++ src/main/java/com/team/shop/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team.shop.model.MemberVO;
 import com.team.shop.service.MemberService;
@@ -17,7 +18,7 @@ public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 		
 	@Autowired
-	private MemberService memberservice;
+	private MemberService memberservice; //@Autowired를 통해 MemberService.java가 MemberController.java에 자동 주입되도록 추가
 	
 	//회원가입 페이지 이동
 	@RequestMapping(value = "/join", method=RequestMethod.GET)
@@ -38,7 +39,7 @@ public class MemberController {
 		
 		logger.info("join Service 성공");
 		
-		return "redirect:/mainlogin";
+		return "redirect:/mainlogin"; //로그인성공 시 메인로그인페이지로 이동
 		
 	}
 	
@@ -49,4 +50,29 @@ public class MemberController {
 		logger.info("로그인 페이지 진입");
 		
 	}
+	
+	// 아이디 중복 검사
+	@RequestMapping(value = "/memberIdChk", method = RequestMethod.POST)
+	@ResponseBody //이 어노테이션이 없을 경우 join.jsp로 결과가 반환되지 않음
+	public String memberIdChkPOST(String memberId) throws Exception{
+		
+		/*logger.info("memberIdChk() 진입");*/
+		
+		int result = memberservice.idCheck(memberId);
+		
+		/*logger.info("결과값 = " + result);*/
+		
+		if(result != 0) { //존재하면 1, 아니면 0 반환
+			
+			return "fail";	// 중복 아이디가 존재
+			
+		} else {
+			
+			return "success";	// 중복 아이디 x
+			
+		}
+		
+		
+	} // memberIdChkPOST() 종료
+	
 }
